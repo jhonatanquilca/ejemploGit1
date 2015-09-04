88d24ecd8ddd4bfbde0e8e02ef3b03e0548fbd8f
jhonatanquilca <jhonatand.quilca@gmail.com>
2015-09-03 22:51:42 -0500

    mas layouts

--- app/src/main/java/com/example/workstation/ejemplogit1/MainActivity.java
+++ app/src/main/java/com/example/workstation/ejemplogit1/MainActivity.java
@@ -11,7 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_main);
-        setContentView(R.layout.fragment_detail);
+//        setContentView(R.layout.fragmento_detail);
+//        setContentView(R.layout.fragmento_main);
+        setContentView(R.layout.fragmento_frorm_create);
+//        setContentView(R.layout.item_list);
 
 
     }
